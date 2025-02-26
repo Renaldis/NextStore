d56778491db24006d0d2bce112678dc8d8914479
Renaldis <renaldis142@gmail.com>
2025-02-26 12:27:35 +0700

    added sample data product, create product card & product price component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,17 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const HomePage = async () => {
-  return <>NextStore</>;
+  return (
+    <div className="space-y-8">
+      {/* <h2 className="h2-bold">Latest Product</h2> */}
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </div>
+  );
 };
 
 export default HomePage;
