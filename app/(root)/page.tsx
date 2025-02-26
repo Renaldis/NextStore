825c4547fc177471bf889c069a1b3628599117e1
Renaldis <renaldis142@gmail.com>
2025-02-26 20:11:46 +0700

    added validator with zod

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,13 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.action";
 
 const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
+
   return (
     <div className="space-y-8">
       {/* <h2 className="h2-bold">Latest Product</h2> */}
-      <ProductList
-        data={sampleData.products}
-        title="Newest Arrivals"
-        limit={4}
-      />
+      <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
     </div>
   );
 };
