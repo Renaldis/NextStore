71073c0742d5c76e241c7e1508730bd0e752534a
Renaldis <renaldis142@gmail.com>
2025-03-07 19:42:24 +0700

    add convertion string to number product rating

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,11 +3,16 @@ import { getLatestProducts } from "@/lib/actions/product.action";
 
 const HomePage = async () => {
   const latestProducts = await getLatestProducts();
+  // konversi rating dari string ke number
+  const products = latestProducts.map((product) => ({
+    ...product,
+    rating: Number(product.rating),
+  }));
 
   return (
     <div className="space-y-8">
       {/* <h2 className="h2-bold">Latest Product</h2> */}
-      <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
+      <ProductList data={products} title="Newest Arrivals" limit={4} />
     </div>
   );
 };
